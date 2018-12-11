a1d87937ef5f50aae52a05697539eaa63bc48403
sgqy <sgqyzh@gmail.com>
2018-12-11 14:34:58 +0800

    change format

--- main.cpp
+++ main.cpp
@@ -27,18 +27,21 @@ class RecFile
 		return out;
 	}
 
-public:
+      public:
 	void load()
 	{
 		fp = fopen(rec_file, "rb");
-		if (fp == 0) {
+		if (fp == 0)
+		{
 			printf("[!] no old record (%s), will create later.\n", rec_file);
 			return;
 		}
 		char key_name[30];
 		int  count = 0;
-		while (1) {
-			if (fscanf(fp, "%s%d", key_name, &count) == EOF) {
+		while (1)
+		{
+			if (fscanf(fp, "%s%d", key_name, &count) == EOF)
+			{
 				break;
 			}
 			rec[key_name] = count;
@@ -48,14 +51,16 @@ public:
 	void p_file()
 	{
 		FILE *fp = fopen(rec_file, "wb+");
-		if (fp == 0) {
+		if (fp == 0)
+		{
 			printf("[!] cannot open and write (%s), will not create.\n", rec_file);
 			return;
 		}
 
 		auto out = sorted();
 
-		for (auto &p : out) {
+		for (auto &p : out)
+		{
 			fprintf(fp, "%s\t%d\n", p.first.c_str(), p.second);
 		}
 		fclose(fp);
@@ -65,7 +70,8 @@ public:
 		auto out   = sorted();
 		int  total = 0;
 
-		for (auto &p : out) {
+		for (auto &p : out)
+		{
 			printf("%15s %6d\n", p.first.c_str(), p.second);
 			total += p.second;
 		}
@@ -99,7 +105,7 @@ class Speed
 		return (uint64_t)(tv.tv_sec) * 1000000 + tv.tv_usec;
 	}
 
-public:
+      public:
 	void init()
 	{
 		count = 0;
@@ -108,7 +114,8 @@ public:
 	void push()
 	{
 		uint64_t dur = cur_time() - last;
-		if (dur < 3 * 1000000) { // ignore types which is too slow
+		if (dur < 3 * 1000000)
+		{ // ignore types which is too slow
 			history[count++ % his_sz] = dur;
 		}
 		last = cur_time();
@@ -117,7 +124,8 @@ public:
 	{
 		uint64_t div = count > his_sz ? his_sz : count;
 		uint64_t sum = 0;
-		for (uint64_t i = 0; i < div; ++i) {
+		for (uint64_t i = 0; i < div; ++i)
+		{
 			sum += history[i];
 		}
 		return (double)div / ((double)sum / 1000000) * 60;
@@ -151,7 +159,8 @@ void dump(int _)
 
 int main(int argc, char **argv)
 {
-	if (argc != 2) {
+	if (argc != 2)
+	{
 		printf("%s <ev-id>\n", argv[0]);
 		printf("To get event ID with name, run `evtest'\n");
 		printf("The ID may be different after reboot\n");
@@ -163,7 +172,8 @@ int main(int argc, char **argv)
 	std::string pn = "sudo evtest /dev/input/event" + std::string(argv[1]) +
 	                 " | stdbuf -o0 grep '(KEY.*1$' | stdbuf -o0 sed -E 's/.*KEY_(.*)\\).*/\\1/g'";
 	FILE *p_ev = popen(pn.c_str(), "r");
-	if (p_ev == 0) {
+	if (p_ev == 0)
+	{
 		return 1;
 	}
 
@@ -179,12 +189,16 @@ int main(int argc, char **argv)
 	Speed ss;
 	ss.init();
 	winsize win;
-	while (alive) {
-		if (fgets(key_name, 30, p_ev) == 0) {
+	while (alive)
+	{
+		if (fgets(key_name, 30, p_ev) == 0)
+		{
 			break;
 		}
-		for (int i = 0; i < 30; ++i) {
-			if (key_name[i] == 0x0A) {
+		for (int i = 0; i < 30; ++i)
+		{
+			if (key_name[i] == 0x0A)
+			{
 				key_name[i] = 0;
 				break;
 			}
@@ -193,14 +207,18 @@ int main(int argc, char **argv)
 		++typed;
 		ss.push();
 		ioctl(STDOUT_FILENO, TIOCGWINSZ, &win);
-		if (typed % (win.ws_col - 35) == 0) {
+		if (typed % (win.ws_col - 35) == 0)
+		{
 			printf("%7d (%6.2lf/min) %s\n", typed, ss.get(), ss.getcur());
-		} else {
+		}
+		else
+		{
 			printf(".");
 		}
 		fflush(stdout);
 
-		if (typed % 2000 == 0) {
+		if (typed % 2000 == 0)
+		{
 			rf.p_file();
 		}
 	}
